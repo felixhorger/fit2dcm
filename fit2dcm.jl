81d1c5e4456f914e13fc2f1a2b3751735b98efa4
Felix Horger <felix.horger@kcl.ac.uk>
2022-02-03 13:03:39 +0000

    Updated dependencies and improved code structure

--- fit2dcm.jl
+++ fit2dcm.jl
@@ -1,10 +1,10 @@
 
 import Statistics
 import YAML
-import MATLAB
+import JLD2
 import DICOM
 import DICOMTools
-import MRIQuant
+import MRIRelax
 
 
 function main()
@@ -13,56 +13,66 @@ function main()
 	for specfile in ARGS
 		# Get specs and DICOMs
 		specs = YAML.load_file(specfile)
-		new_directory = expanduser(specs["directory"])
-		if new_directory != directory # Do not reload unnecessarily
-			directory = new_directory
-			dcmdict = DICOMTools.load_dcms(directory)
+
+		# Load DICOMs
+		multiseries = let
+			directory = expanduser(specs["directory"])
+			dcmdict = DICOMTools.load_dcms(directory, DICOMTools.read_uid)
+
+			# Filter DICOMs by uid and fully load matches
+			dcmdict = DICOMTools.filter(
+				dcms -> dcms[1][DICOM.tag"SeriesInstanceUID"] in specs["uids"],
+				dcmdict
+			)
+			DICOMTools.load_dcms!(dcmdict)
+
+			# Extract DICOMs from dictionary
+			[series.dcms for series in values(dcmdict)]
 		end
 
 		# Get tag and fitfunc
 		if specs["type"] == "Inversion Recovery"
 			tagname = DICOM.tag"InversionTime"
-			T1_0::Float64 = specs["T1_0"] # Type annotations for capturing
-			Minv_0::Float64 = specs["Minv_0"]
-			M0_0::Float64 = specs["M0_0"]
+			tags = DICOMTools.get_tag(multiseries, tagname; outtype=Float64)
+			# Note: Get tags here because some applications might use other (non-numeric, multiple) tags
 			fitfunc = (Tinv, signal, Δsignal) -> begin
-				MRIQuant.fit_inversion_recovery(Tinv, signal, Δsignal, T1_0, Minv_0, M0_0)
+				let	T1_0::Float64 = specs["T1_0"],
+					Minv_0::Float64 = specs["Minv_0"],
+					M0_0::Float64 = specs["M0_0"]
+					MRIRelax.fit_inversion_recovery(Tinv, signal, Δsignal, T1_0, Minv_0, M0_0)
+				end
 			end
-			returntypes = (Float64, Float64, Float64, Float64, Float64, Float64)
-			names = ("T1", "DeltaT1", "Minv", "DeltaMinv", "M0", "DeltaM0")
+			num_params = 6
+			names = ["T1", "ΔT1", "Minv", "ΔMinv", "M0", "ΔM0"]
 		elseif specs["type"] == "Transverse Relaxation"
 			tagname = DICOM.tag"EchoTime"
-			fitfunc = MRIQuant.fit_transverse_relax
-			returntypes = (Float64, Float64, Float64, Float64)
-			names = ("T2", "DeltaT2", "M0", "DeltaM0")
+			tags = DICOMTools.get_tag(multiseries, tagname; outtype=Float64)
+			fitfunc = MRIRelax.fit_transverse_relax
+			num_params = 4
+			names = ["T2", "ΔM0", "ΔM0"]
 		end
 
-		# Get background
-		background = [
-			l:u
-			for (l, u) in zip(specs["background_lower"], specs["background_upper"])
-		]
+		# Get signals and tags from DICOMs
+		signal = DICOMTools.merge_multiseries(multiseries; data_type=Float64)
 
-		# Get arrays and tags from DICOMs
-		arrays, tags = DICOMTools.merge_multiseries(
-			dcmdict,
-			specs["uids"],
-			tagname;
-			data_type=Float64,
-			tag_type=Float64
+		# Get background
+		background = ntuple(
+			i -> specs["background_lower"][i]:specs["background_upper"][i],
+			3
 		)
-		
+
 		# Perform the fitting
-		maps, Δsignal = fit2dcm(fitfunc, returntypes, arrays, tags, background)
+		maps, Δsignal = fit2dcm(fitfunc, num_params, signal, tags, background)
 
 		# Write the results
-		MATLAB.write_matfile(
-			splitext(specfile)[1] * ".mat",
-			maps=maps,
-			names=names,
-			coordinated=tags,
+		JLD2.jldsave(
+			splitext(specfile)[1] * ".jld2";
+			maps,
+			names,
+			coordinates=tags,
 			coordinatename=tagname,
-			deltasignal=Δsignal
+			signal,
+			Δsignal
 		)
 	end
 end
@@ -70,26 +80,26 @@ end
 
 function fit2dcm(
 	fitfunc::Function,
-	returntypes::NTuple{N, Type} where N,
-	arrays::AbstractArray{<: Real, 4},
+	num_params::Integer,
+	signal::AbstractArray{<: Real, 4},
 	tags::AbstractVector,
-	background::AbstractVector{<: UnitRange{Int64}}
+	background::NTuple{3, UnitRange{Int64}}
 )
 	
 	# Get noise estimation for each signal(coordinate or tag)
-	Δsignal = Statistics.std(view(arrays, :, background...); dims=2:4)
+	Δsignal = Statistics.std(view(signal, :, background...); dims=2:4)
 	Δsignal = dropdims(Δsignal; dims=(2, 3, 4))
 
 	# Construct array for fitting results
 	maps = [
-		Array{T, 3}(undef, size(arrays)[2:4]...)
-		for T in returntypes
+		Array{Float64, 3}(undef, size(signal)[2:4]...)
+		for _ in 1:num_params
 	]
 
 	# Iterate over voxels
-	Threads.@threads for I in CartesianIndices(size(arrays)[2:4])
+	Threads.@threads for I in CartesianIndices(size(signal)[2:4])
 		tid = Threads.threadid()
-		returned = fitfunc(tags, arrays[:, I], Δsignal)
+		returned = fitfunc(tags, signal[:, I], Δsignal)
 		for (tag_idx, value) in enumerate(returned)
 			maps[tag_idx][I] = value
 		end
